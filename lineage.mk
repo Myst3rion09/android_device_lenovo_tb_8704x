89f112d33afc4928a039c310d9d76cc096d92c03
Julian Fischer <myst3rion9@gmail.com>
2017-12-22 20:24:17 +0100

    tb_8704x: Update lineage.mk
    - Remove full_tb_8704x.mk
    - Update build fingerprint
    
    Change-Id: I0f87e0cb18cd6d8c7538e0cd7bd1c4ad6c2a1dd5

--- lineage.mk
+++ lineage.mk
@@ -14,16 +14,30 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/lenovo/tb_8704x/full_tb_8704x.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1080
+TARGET_SCREEN_WIDTH := 1920
+
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/telephony.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/lenovo/tb_8704x/device.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_tb_8704x
-BOARD_VENDOR := Lenovo
+PRODUCT_DEVICE := tb_8704x
+PRODUCT_BRAND := Lenovo
+PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_MODEL := Lenovo Tab4 8 Plus
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Lenovo/TB-8704X/TB-8704X:7.1.1/NMF26F/TB-8704X_USR_S026_170707_Q1241_ROW:user/release-keys" \
-    PRIVATE_BUILD_DESC="msm8953_64-user 7.1.1 NMF26F 393 release-keys"
+    BUILD_FINGERPRINT="Lenovo/TB-8704X/TB-8704X:7.1.1/NMF26F/TB-8704X_USR_S033_171123_Q1241_ROW:user/release-keys"\
+    PRIVATE_BUILD_DESC="msm8953_64-user 7.1.1 NMF26F 609 release-keys"
